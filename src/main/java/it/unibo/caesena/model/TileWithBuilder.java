19a1f0f952c261ae1d1db733c14ca26b022f927f
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-02-28 10:58:02 +0100

    Create TileWithBuilder class

---
+++ src/main/java/it/unibo/caesena/model/TileWithBuilder.java
@@ -0,0 +1,40 @@
+package it.unibo.caesena.model;
+
+import java.util.Optional;
+
+import it.unibo.caesena.utils.Position;
+
+public class TileWithBuilder implements Tile {
+
+    @Override
+    public void rotateClockwise() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'rotateClockwise'");
+    }
+
+    @Override
+    public Optional<Position> getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public void setPosition(Position pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+    }
+
+    @Override
+    public boolean isPlaced() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isPlaced'");
+    }
+
+    @Override
+    public String getImagePath() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getImagePath'");
+    }
+    
+    public static class Builder {}
+}
