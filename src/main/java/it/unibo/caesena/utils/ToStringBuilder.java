ac1d71bb56f2be223cea1bfde49cab0835292420
Davide Speziali <davide.speziali@studio.unibo.it>
2023-02-28 18:22:14 +0100

    Update ToStringBuilder class to force capitalization and add ability to use objects as values and add support for null strings

--- src/main/java/it/unibo/caesena/utils/ToStringBuilder.java
+++ src/main/java/it/unibo/caesena/utils/ToStringBuilder.java
@@ -10,9 +10,34 @@ public class ToStringBuilder {
         this.elements = new ArrayList<>();
     }
 
-    public ToStringBuilder add(String field, String value) {
-        elements.add(new Pair<String,String>(field, value));
+    public ToStringBuilder add(final String field, final String value) {
+        String actualValue = String.valueOf("");
+        if (value == null) {
+            actualValue = "null";
+        } else {
+            actualValue = value;
+        }
+        Pair<String, String> definitivePair = new Pair<String,String>(capitalize(field), capitalize(actualValue));
+        elements.add(definitivePair);
         return this;
+    }
+
+    public ToStringBuilder add(final String field, final Object value) {
+        String actualValue = String.valueOf("");
+        if (value == null) {
+            actualValue = "null";
+        } else {
+            actualValue = value.toString();
+        }
+        Pair<String, String> definitivePair = new Pair<String,String>(capitalize(field), capitalize(actualValue));
+        elements.add(definitivePair);
+        return this;
+    }
+
+    private String capitalize(String string) {
+        char[] charArray = string.toLowerCase().toCharArray();
+        charArray[0] = String.valueOf(charArray[0]).toUpperCase().charAt(0);
+        return String.valueOf(charArray);
     }
 
     public String build()
