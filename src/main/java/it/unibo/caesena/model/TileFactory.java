693b1ec23a8baae53ee33647fbac65398d675905
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-02-28 19:41:40 +0100

    Create TileFactory interface and implement functions for the CityEdge and CityLarge tiles

--- src/main/java/it/unibo/caesena/model/TileFactory.java
+++ src/main/java/it/unibo/caesena/model/TileFactory.java
@@ -2,4 +2,42 @@ package it.unibo.caesena.model;
 
 public interface TileFactory {
     
+    public Tile createCityEdge();
+
+    public Tile createCityEdgeRoad();
+    
+    public Tile createCityLarge();
+    
+    public Tile createCityLargeRoad();
+    
+    public Tile createCity();
+    
+    public Tile createCitySideDoubleNext();
+    
+    public Tile createCitySideDouble();
+    
+    public Tile createCitySideJunction();
+    
+    public Tile createCitySide();
+    
+    public Tile createCitySideRoad();
+    
+    public Tile createCitySideTurnLeft();
+    
+    public Tile createCitySideTurnRight();
+    
+    public Tile createCityTube();
+    
+    public Tile createMonastery();
+    
+    public Tile createMonasteryRoad();
+    
+    public Tile createRoadJunctionLarge();
+    
+    public Tile createRoadJunctionSmall();
+    
+    public Tile createRoadStraight();
+    
+    public Tile createRoadTurn();
+
 }
--- src/main/java/it/unibo/caesena/model/TileFactoryWithBuilder.java
+++ src/main/java/it/unibo/caesena/model/TileFactoryWithBuilder.java
@@ -1,5 +1,139 @@
 package it.unibo.caesena.model;
 
+import java.util.List;
+
 public class TileFactoryWithBuilder implements TileFactory {
+
+    @Override
+    public Tile createCityEdge() {
+        return new TileBuilder("city-edge")
+            .city(List.of(TileSection.RightUp, TileSection.RightCenter, TileSection.RightDown, 
+                TileSection.UpLeft, TileSection.UpCenter, TileSection.UpRight))
+            .field(List.of(TileSection.DownLeft, TileSection.DownCenter, TileSection.DownRight, 
+            TileSection.LeftUp, TileSection.LeftCenter, TileSection.LeftDown))
+            .build();
+    }
+
+    @Override
+    public Tile createCityEdgeRoad() {
+        return new TileBuilder("city-edge-road")
+            .city(List.of(TileSection.RightUp, TileSection.RightCenter, TileSection.RightDown, 
+                TileSection.UpLeft, TileSection.UpCenter, TileSection.UpRight))
+            .field(List.of(TileSection.DownLeft, TileSection.DownCenter, TileSection.DownRight, 
+                TileSection.LeftUp, TileSection.LeftCenter, TileSection.LeftDown))
+            .road(List.of(TileSection.LeftCenter, TileSection.DownCenter))
+            .build();
+    }
+
+    @Override
+    public Tile createCityLarge() {
+        return new TileBuilder("city-large")
+            .city(List.of(TileSection.RightUp, TileSection.RightCenter, TileSection.RightDown, 
+                TileSection.UpLeft, TileSection.UpCenter, TileSection.UpRight,
+                TileSection.LeftUp, TileSection.LeftCenter, TileSection.LeftDown))
+            .field(List.of(TileSection.DownLeft, TileSection.DownCenter, TileSection.DownRight))
+            .build();
+    }
+
+    @Override
+    public Tile createCityLargeRoad() {
+        return new TileBuilder("city-large-road")
+            .city(List.of(TileSection.RightUp, TileSection.RightCenter, TileSection.RightDown, 
+                TileSection.UpLeft, TileSection.UpCenter, TileSection.UpRight,
+                TileSection.LeftUp, TileSection.LeftCenter, TileSection.LeftDown))
+            .field(List.of(TileSection.DownLeft, TileSection.DownRight))
+            .road(List.of(TileSection.DownCenter))
+            .build();
+    }
+
+    @Override
+    public Tile createCity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCity'");
+    }
+
+    @Override
+    public Tile createCitySideDoubleNext() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCitySideDoubleNext'");
+    }
+
+    @Override
+    public Tile createCitySideDouble() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCitySideDouble'");
+    }
+
+    @Override
+    public Tile createCitySideJunction() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCitySideJunction'");
+    }
+
+    @Override
+    public Tile createCitySide() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCitySide'");
+    }
+
+    @Override
+    public Tile createCitySideRoad() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCitySideRoad'");
+    }
+
+    @Override
+    public Tile createCitySideTurnLeft() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCitySideTurnLeft'");
+    }
+
+    @Override
+    public Tile createCitySideTurnRight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCitySideTurnRight'");
+    }
+
+    @Override
+    public Tile createCityTube() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCityTube'");
+    }
+
+    @Override
+    public Tile createMonastery() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createMonastery'");
+    }
+
+    @Override
+    public Tile createMonasteryRoad() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createMonasteryRoad'");
+    }
+
+    @Override
+    public Tile createRoadJunctionLarge() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRoadJunctionLarge'");
+    }
+
+    @Override
+    public Tile createRoadJunctionSmall() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRoadJunctionSmall'");
+    }
+
+    @Override
+    public Tile createRoadStraight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRoadStraight'");
+    }
+
+    @Override
+    public Tile createRoadTurn() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createRoadTurn'");
+    }
     
 }
