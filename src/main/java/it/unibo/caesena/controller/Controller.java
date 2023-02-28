8279f66457a358fc2b7b0b3a5e2e050beb748058
Davide Speziali <davide.speziali@studio.unibo.it>
2023-02-28 18:47:30 +0100

    Create Controllerinterface

--- src/main/java/it/unibo/caesena/controller/Controller.java
+++ src/main/java/it/unibo/caesena/controller/Controller.java
@@ -1,5 +1,37 @@
 package it.unibo.caesena.controller;
 
+import java.util.List;
+
+import it.unibo.caesena.model.*;
+import it.unibo.caesena.utils.*;
+
 public interface Controller {
-    
+
+    int startGame();
+
+    void addPlayer(String name, Color color);
+
+    boolean placeMeeple(Meeple meeple);
+
+    Tile getCurrentTile();
+
+    void rotateCurrentTile();
+
+    boolean placeCurrentTile();
+
+    List<Player> getPlayers();
+
+    List<Tile> getPlacedTiles();
+
+    List<Meeple> getMeeples();
+
+    boolean isGameOver();
+
+    void endTurn();
+
+    void endGame();
+
+    void exitGame();
+
+    void saveGame();
 }
