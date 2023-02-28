121d4ba8cdf3df768e03007a7aa2d24aa75eb1fc
Samuele-giancarli <samugianca@gmail.com>
2023-02-28 17:32:20 +0100

    Implement TileSection

--- src/main/java/it/unibo/caesena/model/TileSection.java
+++ src/main/java/it/unibo/caesena/model/TileSection.java
@@ -1,5 +1,17 @@
 package it.unibo.caesena.model;
 
 public enum TileSection {
-    
+    UpCenter,
+    UpLeft,
+    UpRight,
+    DownCenter,
+    DownLeft,
+    DownRight,
+    LeftCenter,
+    LeftUp,
+    LeftDown,
+    RightCenter,
+    RightUp,  
+    RightDown,
+    Center
 }
