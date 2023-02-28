5aa7c091a9667d391299fd7680092ae1da5f5c30
Samuele-giancarli <samugianca@gmail.com>
2023-02-28 10:29:41 +0100

    Create Tile interface

---
+++ src/main/java/it/unibo/caesena/model/Tile.java
@@ -0,0 +1,17 @@
+package it.unibo.caesena.model;
+
+import java.util.Optional;
+
+import it.unibo.caesena.utils.Position;
+
+public interface Tile {
+
+    public void rotateClockwise();
+
+    public Optional<Position> getPosition();
+    public void setPosition(Position pos);
+
+    public boolean isPlaced();
+
+    public String getImagePath();
+}
