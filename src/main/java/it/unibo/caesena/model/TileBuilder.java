7e354ad0ccbcd3b27a28cd04506eae709b781692
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-02-28 17:23:54 +0100

    Complete TileBuilder

--- src/main/java/it/unibo/caesena/model/TileBuilder.java
+++ src/main/java/it/unibo/caesena/model/TileBuilder.java
@@ -1,5 +1,43 @@
 package it.unibo.caesena.model;
 
+import java.util.List;
+
 public class TileBuilder {
     
+    private final Tile tile;
+
+    public TileBuilder(String imagePath) {
+        this.tile = new TileImpl(imagePath);
+    }
+
+    public TileBuilder city(List<TileSection> sections) {
+        applySet(sections, new CitySet());
+        return this;
+    }
+
+    public TileBuilder road(List<TileSection> sections) {
+        applySet(sections, new RoadSet());
+        return this;
+    }
+
+    public TileBuilder field(List<TileSection> sections) {
+        applySet(sections, new FieldSet());
+        return this;
+    }
+
+    public TileBuilder monastery(List<TileSection> sections) {
+        applySet(sections, new MonasterySet());
+        return this;
+    }
+
+    private void applySet(List<TileSection> sections, GameSet gameSet) {
+        for (var section : sections) {
+            this.tile.putSection(section, gameSet);
+        }
+    }
+
+    public Tile build() {
+        return this.tile;
+    }
+
 }
