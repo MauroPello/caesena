c593e761a516e85d90649eaadbb3120f2933caf2
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-01 11:59:44 +0100

    Create NormalMeepleTest

---
+++ src/test/java/it/unibo/caesena/NormalMeepleTest.java
@@ -0,0 +1,56 @@
+package it.unibo.caesena;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.caesena.model.NormalMeeple;
+import it.unibo.caesena.model.Player;
+import it.unibo.caesena.model.PlayerImpl;
+import it.unibo.caesena.model.Tile;
+import it.unibo.caesena.model.TileFactoryWithBuilder;
+import it.unibo.caesena.model.TileSection;
+import it.unibo.caesena.utils.Color;
+
+final class NormalMeepleTest {
+
+    private static NormalMeeple meeple;
+    private static Player owner;
+
+    @BeforeAll
+    public static void init() {
+        owner = new PlayerImpl("Giocatore1", Color.createColor("FF0000", "Red"));
+        meeple = new NormalMeeple(owner);
+    }
+
+    @Test
+    public void testGetters() {
+        assertEquals(1, meeple.getStrength());
+        assertEquals(Optional.empty(), meeple.getTileSection());
+        assertEquals(Optional.empty(), meeple.getTile());
+        assertFalse(meeple.isPlaced());
+        assertFalse(meeple.removeFromTile());
+        assertEquals(owner, meeple.getOwner());
+    }
+
+    @Test
+    public void testEquals() {
+        final NormalMeeple meeple2 = new NormalMeeple(owner);
+        assertEquals(meeple2, meeple);
+    }
+
+    @Test
+    public void testPlace() {
+        final Tile tile = new TileFactoryWithBuilder().createMonastery();
+        assertTrue(meeple.place(TileSection.Center, tile));
+        assertEquals(TileSection.Center, meeple.getTileSection().get());
+        assertEquals(tile, meeple.getTile().get());
+        assertTrue(meeple.removeFromTile());
+    }
+
+}
