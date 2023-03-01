cdf64da5bb46f3ef96a893b4798e1c4542d86166
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-01 12:17:46 +0100

    Add meeple functions to GameSet

--- src/main/java/it/unibo/caesena/model/GameSet.java
+++ src/main/java/it/unibo/caesena/model/GameSet.java
@@ -2,4 +2,10 @@ package it.unibo.caesena.model;
 
 public interface GameSet {
     
+    public boolean addMeeple(Meeple meeple);
+
+    public boolean removeMeeple(Meeple meeple);
+
+    public boolean isMeepleFree();
+
 }
--- src/main/java/it/unibo/caesena/model/RoadSet.java
+++ src/main/java/it/unibo/caesena/model/RoadSet.java
@@ -1,5 +1,23 @@
 package it.unibo.caesena.model;
 
 public class RoadSet implements GameSet {
+
+    @Override
+    public boolean addMeeple(Meeple meeple) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addMeeple'");
+    }
+
+    @Override
+    public boolean removeMeeple(Meeple meeple) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeMeeple'");
+    }
+
+    @Override
+    public boolean isMeepleFree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isMeepleFree'");
+    }
     
 }
