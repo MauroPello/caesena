cd92dd3da443959f9c710a2138dfbcff8f657770
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-02 19:00:03 +0100

    Update UserInterface by adding all the views

--- src/main/java/it/unibo/caesena/view/UserInterface.java
+++ src/main/java/it/unibo/caesena/view/UserInterface.java
@@ -8,29 +8,49 @@ import it.unibo.caesena.controller.ControllerImpl;
 public class UserInterface extends JFrame {
     
     private final Controller controller;
+    private final View startView;
+    private final View gameView;
+    private final View pauseView;
+    private final View gameOverView;
 
     public UserInterface() {
         super();
         this.controller = new ControllerImpl();
         // TODO set options for JFrame
-
+        this.startView = new StartView();
+        this.gameView = new GameView();
+        this.pauseView = new PauseView();
+        this.gameOverView = new GameOverView();
     }
 
     public void showStartView() {
+        hideAllViews();
+        startView.show();
 
     }
 
     public void showGameView() {
+        hideAllViews();
+        gameView.show();
 
     }
 
     public void showPauseView() {
+        hideAllViews();
+        pauseView.show();
 
     }
 
     public void showGameOverView() {
+        hideAllViews();
+        gameOverView.show();
 
     }
 
-    
+    private void hideAllViews() {
+        startView.hide();
+        gameView.hide();
+        pauseView.hide();
+        gameOverView.hide();
+    }
 }
