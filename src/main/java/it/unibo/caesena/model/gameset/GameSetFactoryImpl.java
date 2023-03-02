a82da3a36eb53c90f422e19aa667d0f6fa236a3c
Alessandro Martini <alessandro.martini10@studio.unibo.it>
2023-03-02 17:43:18 +0100

    Add GameSetTest & update GameSetFactory Impl

--- src/main/java/it/unibo/caesena/model/gameset/GameSetFactoryImpl.java
+++ src/main/java/it/unibo/caesena/model/gameset/GameSetFactoryImpl.java
@@ -31,20 +31,26 @@ public class GameSetFactoryImpl implements GameSetFactory {
 
     @Override
     public GameSet createJoinedSet(GameSet gs1, GameSet gs2) {
-        var firstPair = gs1.close();
-        var secondPair = gs2.close();
-        GameSetType newTipe = gs1.getType();
-        int comulativePoints = firstPair.getY() + secondPair.getY();
-        GameSet joinedGameset = new GameSetImpl(newTipe, comulativePoints);
         
-        for (var meeple : firstPair.getX()) {
-            joinedGameset.addMeeple(meeple);
-        }
-       
-        for (var meeple : secondPair.getX()) {
-            joinedGameset.addMeeple(meeple);
-        }
+        if (gs1.getType().equals(gs2.getType())) {
+            
+            var firstPair = gs1.close();
+            var secondPair = gs2.close();
+            GameSetType newTipe = gs1.getType();
+            int comulativePoints = firstPair.getY() + secondPair.getY();
+            GameSet joinedGameset = new GameSetImpl(newTipe, comulativePoints);
+            
+            for (var meeple : firstPair.getX()) {
+                joinedGameset.addMeeple(meeple);
+            }
         
-        return joinedGameset;
+            for (var meeple : secondPair.getX()) {
+                joinedGameset.addMeeple(meeple);
+            }
+            
+            return joinedGameset;
+        } else {
+            throw new IllegalStateException("The type's of GameSet are different");
+        }
     }
 }
---
+++ src/test/java/it/unibo/caesena/GameSetTest.java
@@ -0,0 +1,45 @@
+package it.unibo.caesena;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.caesena.model.gameset.GameSet;
+import it.unibo.caesena.model.gameset.GameSetFactory;
+import it.unibo.caesena.model.gameset.GameSetFactoryImpl;
+
+final class GameSetFactoryImplTest {
+
+    private static GameSetFactory gamesetFactory;    
+    private static GameSet gamesetCity;    
+    private static GameSet gamesetMonastery;
+    private static GameSet gamesetRoad;
+    private static GameSet gamesetField;
+
+    @BeforeAll
+    public static void init() {
+        gamesetFactory = new GameSetFactoryImpl();
+        gamesetCity = gamesetFactory.createCitySet();
+        gamesetField =  gamesetFactory.createFieldSet();
+        gamesetMonastery =  gamesetFactory.createMonasterySet();
+        gamesetRoad =  gamesetFactory.createRoadSet();
+    }
+
+    @Test
+    public void testCreateJoinedSet(){
+        assertThrows(IllegalStateException.class, () -> gamesetFactory.createJoinedSet(gamesetField, gamesetCity));
+        assertThrows(IllegalStateException.class, () -> gamesetFactory.createJoinedSet(gamesetField, gamesetMonastery));
+        assertThrows(IllegalStateException.class, () -> gamesetFactory.createJoinedSet(gamesetField, gamesetRoad));
+        assertThrows(IllegalStateException.class, () -> gamesetFactory.createJoinedSet(gamesetRoad, gamesetCity));
+        assertThrows(IllegalStateException.class, () -> gamesetFactory.createJoinedSet(gamesetMonastery, gamesetCity));
+        assertThrows(IllegalStateException.class, () -> gamesetFactory.createJoinedSet(gamesetRoad, gamesetMonastery));
+        
+        assertEquals(gamesetCity, gamesetFactory.createJoinedSet(gamesetCity, gamesetCity));
+        assertEquals(gamesetField, gamesetFactory.createJoinedSet(gamesetField, gamesetField));
+        assertEquals(gamesetRoad, gamesetFactory.createJoinedSet(gamesetRoad, gamesetRoad));
+        assertEquals(gamesetMonastery, gamesetFactory.createJoinedSet(gamesetMonastery, gamesetMonastery));
+    }
+}
