d5bd07c9e1a055d1a8e4c20f7d27f90fb59b00ef
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-03 17:51:11 +0100

    Update UserInterface constructor

--- src/main/java/it/unibo/caesena/view/UserInterface.java
+++ src/main/java/it/unibo/caesena/view/UserInterface.java
@@ -1,5 +1,8 @@
 package it.unibo.caesena.view;
 
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 import it.unibo.caesena.controller.Controller;
@@ -16,10 +19,24 @@ public class UserInterface extends JFrame {
         super();
         this.controller = controller;
         // TODO set options for JFrame
+
         this.startView = new StartView(this);
         this.gameView = new GameView(this);
         this.pauseView = new PauseView(this);
         this.gameOverView = new GameOverView(this);
+
+        this.getContentPane().add(startView);
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        this.setSize(sw / 3, sh / 3);
+        this.setLocationByPlatform(true);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        showStartView();
     }
 
     public void showStartView() {
