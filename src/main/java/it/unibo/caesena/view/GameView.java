977577d5c097fc0b9ec80d8c5cb58092cbaae8eb
Davide Speziali <davide.speziali@studio.unibo.it>
2023-03-03 17:37:11 +0100

    Update GameView class

--- src/main/java/it/unibo/caesena/view/GameView.java
+++ src/main/java/it/unibo/caesena/view/GameView.java
@@ -1,6 +1,14 @@
 package it.unibo.caesena.view;
 
+import javax.swing.JButton;
+
+import it.unibo.caesena.controller.Controller;
+
 public class GameView implements View {
+    private final static int DEFAULT_ZOOM_LEVEL = 10;
+
+    private Controller controller; //TODO final?
+    private int currentZoomLevel = DEFAULT_ZOOM_LEVEL;
 
     @Override
     public void show() {
@@ -12,6 +20,20 @@ public class GameView implements View {
     public void hide() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'hide'");
+    }
+
+    public void drawField() {
+        for (int i = 0; i < currentZoomLevel; i++) {
+            
+        }
+    }
+
+    public void drawMapControls() {
+        
+    }
+
+    public void drawTableTop() {
+
     }
 
 }
