5988129111043db6134b4361c6d3489f81b9ae7e
Davide Speziali <davide.speziali@studio.unibo.it>
2023-03-07 18:45:47 +0100

    Create BoardComponent interface

---
+++ src/main/java/it/unibo/caesena/view/components/BoardComponent.java
@@ -0,0 +1,19 @@
+package it.unibo.caesena.view.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public interface BoardComponent {
+
+    void zoomIn();
+
+    void zoomOut();
+
+    void moveUp();
+
+    void moveDown();
+
+    void moveLeft();
+
+    void moveRight();
+}
