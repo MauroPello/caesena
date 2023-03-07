7107591736c19f725e7ef46e006425674d98eed0
Davide Speziali <davide.speziali@studio.unibo.it>
2023-03-07 18:53:49 +0100

    Create BoardComponentImpl that implements BoardComponent and extends java.awt.Component

---
+++ src/main/java/it/unibo/caesena/view/components/BoardComponentImpl.java
@@ -0,0 +1,43 @@
+package it.unibo.caesena.view.components;
+
+import java.awt.*;
+
+public class BoardComponentImpl extends Component implements BoardComponent {
+
+    @Override
+    public void zoomIn() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'zoomIn'");
+    }
+
+    @Override
+    public void zoomOut() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'zoomOut'");
+    }
+
+    @Override
+    public void moveUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveUp'");
+    }
+
+    @Override
+    public void moveDown() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveDown'");
+    }
+
+    @Override
+    public void moveLeft() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+    }
+
+    @Override
+    public void moveRight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+    }
+
+}
