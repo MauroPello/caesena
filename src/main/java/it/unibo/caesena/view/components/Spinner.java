b4b4958ec1cc458a1dd635bf9bbc684b74ae4449
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-09 10:00:33 +0100

    Create custom JSpinner to be used by StartView

---
+++ src/main/java/it/unibo/caesena/view/components/Spinner.java
@@ -0,0 +1,34 @@
+package it.unibo.caesena.view.components;
+
+import java.awt.Color;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+public class Spinner extends JSpinner {
+    
+    private final SpinnerNumberModel model;
+    private final DefaultEditor editor;
+
+    public Spinner(int start, int min, int max, int step) {
+        super();
+
+        model = new SpinnerNumberModel(start, min, max, step);
+        this.setModel(model);
+
+        editor = new DefaultEditor(this);
+        getTextField().setEnabled(false);
+        getTextField().setDisabledTextColor(Color.BLACK);
+        this.setEditor(editor);
+    }
+
+    public int getNumberAsInt() {
+        return model.getNumber().intValue();
+    }
+
+    public JFormattedTextField getTextField() {
+        return editor.getTextField();
+    }
+
+}
