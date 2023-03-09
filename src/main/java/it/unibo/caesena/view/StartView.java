8212247b2602410a4940554bb446b2ae12c88c0b
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-09 10:23:44 +0100

    Add PlayerInput to StartView

--- src/main/java/it/unibo/caesena/view/StartView.java
+++ src/main/java/it/unibo/caesena/view/StartView.java
@@ -3,13 +3,16 @@ package it.unibo.caesena.view;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import it.unibo.caesena.view.components.Spinner;
+import it.unibo.caesena.view.components.NumericUpDown;
+import it.unibo.caesena.view.components.PlayerInput;
 
 public class StartView extends View {
 
@@ -17,13 +20,16 @@ public class StartView extends View {
     private final static int MAX_PLAYERS = 8;
 
     private final JPanel playersNumPanel;
-    private final Spinner playersNumSpinner;
+    private final NumericUpDown playersNum;
+    private final JPanel playerSelectionPanel;
+    private final List<PlayerInput> playerPanels;
 
     private Font mainFont;
 
     public StartView(GUI userInterface) {
         super(userInterface);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.playerPanels = new ArrayList<>();
         
         this.mainFont = new Font("SansSerif", Font.BOLD, 20);
 
@@ -32,18 +38,50 @@ public class StartView extends View {
         playersLabel.setFont(mainFont);
         playersNumPanel.add(playersLabel);
 
-        playersNumSpinner = new Spinner(MIN_PLAYERS, MIN_PLAYERS, MAX_PLAYERS, 1);
-        playersNumSpinner.setSize(new Dimension(50, 50));
-        playersNumSpinner.getTextField().setFont(mainFont);
-        playersNumPanel.add(playersNumSpinner);
+        playersNum = new NumericUpDown(MIN_PLAYERS, MIN_PLAYERS, MAX_PLAYERS, 1);
+        playersNum.setSize(new Dimension(50, 50));
+        playersNum.addChangeListener((e) -> {
+            if (playersNum.getNumberAsInt() < playerPanels.size()) {
+                while(playerPanels.size() > playersNum.getNumberAsInt()) {
+                    removePlayerInput();
+                }
+            } else {
+                while(playerPanels.size() < playersNum.getNumberAsInt()) {
+                    addPlayerInput();
+                }
+            }
+
+            this.repaint();
+            this.validate();
+        });
+        playersNum.getTextField().setFont(mainFont);
+        playersNumPanel.add(playersNum);
         
         playersNumPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.add(playersNumPanel);
+
+        playerSelectionPanel = new JPanel();
+        playerSelectionPanel.setLayout(new BoxLayout(playerSelectionPanel, BoxLayout.Y_AXIS));
+        for (int i = 0; i < MIN_PLAYERS; i++) {
+            addPlayerInput();
+        }
+        this.add(playerSelectionPanel);
 
         JButton startButton = new JButton("Start Game");
         startButton.addActionListener((e) -> {
             userInterface.showGameView();
         });
         this.add(startButton);
+    }
+
+    private void addPlayerInput() {
+        var playerPanel = new PlayerInput();
+
+        playerSelectionPanel.add(playerPanel);
+        this.playerPanels.add(playerPanel);
+    }
+
+    private void removePlayerInput() {
+        playerSelectionPanel.remove(playerPanels.remove(this.playerPanels.size() - 1));
     }
 }
