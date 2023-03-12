a4b1488bdf4a652a042864ddde081d73885850c7
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-12 20:21:11 +0100

    Update PlayerInput, now using JColorChooser to pick color

--- src/main/java/it/unibo/caesena/view/components/PlayerInput.java
+++ src/main/java/it/unibo/caesena/view/components/PlayerInput.java
@@ -1,5 +1,12 @@
 package it.unibo.caesena.view.components;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -10,8 +17,13 @@ public class PlayerInput extends JPanel {
     
     private final static int TEXT_FIELD_COLUMNS = 4;
 
+    private final JColorChooser playerColorChooser;
+    private final JDialog playerColorDialog;
+    private final JPanel playerColorPanel;
+    private final JButton playerColorBtn;
     private final JTextField playerName;
-    private final JTextField playerColor;
+    
+    private Color playerColor;
 
     public PlayerInput() {
         super();
@@ -22,14 +34,30 @@ public class PlayerInput extends JPanel {
         this.add(playerName);
         
         this.add(new JLabel("Color: "));
-        this.playerColor = new JTextField();
-        this.playerColor.setColumns(TEXT_FIELD_COLUMNS);
-        this.add(playerColor);
+        this.playerColorPanel = new JPanel();
+        this.playerColorPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        this.playerColorPanel.setPreferredSize(new Dimension(50,50));
+        this.playerColorPanel.setMinimumSize(new Dimension(50,50));
+        this.playerColorPanel.addPropertyChangeListener("background", (p) -> {
+            this.playerColor = (Color)p.getNewValue();
+        });
+        this.playerColorPanel.setBackground(getBackground());
+        this.add(this.playerColorPanel);
 
+        this.playerColorChooser = new JColorChooser();
+        this.playerColorChooser.setPreviewPanel(new JPanel());
+
+        this.playerColorDialog = JColorChooser.createDialog(this, "Pick a Color!", true, this.playerColorChooser, 
+            (e) -> this.playerColorPanel.setBackground(this.playerColorChooser.getColor()), 
+            (e) -> this.playerColorPanel.setBackground(getBackground()));
+
+        this.playerColorBtn = new JButton("Pick");
+        this.playerColorBtn.addActionListener((e) -> this.playerColorDialog.setVisible(true));
+        this.add(playerColorBtn);
     }
 
     public Pair<String, String> getPlayerData() {
-        return new Pair<>(playerName.getText(), playerColor.getText());
+        return new Pair<>(playerName.getText(), Integer.toHexString((playerColor.getRGB() & 0xffffff) | 0x1000000).substring(1));
     }
 
 }
