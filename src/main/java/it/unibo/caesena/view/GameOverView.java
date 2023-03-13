24176fbbd6bc3d1373f25e0efdd799fe6a1880ad
Alessandro Martini <alessandro.martini10@studio.unibo.it>
2023-03-13 11:34:53 +0100

    Create GameOverView class

--- src/main/java/it/unibo/caesena/view/GameOverView.java
+++ src/main/java/it/unibo/caesena/view/GameOverView.java
@@ -1,9 +1,37 @@
 package it.unibo.caesena.view;
 
-public class GameOverView extends View {
+import java.util.List;
+import javax.swing.*;
 
-    public GameOverView(GUI userInterface) {
+import java.awt.Component;
+import java.awt.Font;
+
+import it.unibo.caesena.model.Player;
+
+public class GameOverView extends View{
+
+    private final JPanel finalPanel;
+    private final List<Player> numberOfPlayers;
+    private Font endMainFont;
+
+    public GameOverView (GUI userInterface) {
         super(userInterface);
-    }
+        
+        this.finalPanel = new JPanel();
+        this.numberOfPlayers = userInterface.getController().getPlayers();
 
+        JLabel playersLabel = new JLabel("Players: ");
+        this.finalPanel.add(playersLabel);
+
+        this.finalPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        
+        for (int i = 0; i < this.numberOfPlayers.size(); i++) {
+            JLabel newLabel = new JLabel("" + this.numberOfPlayers.get(i).getName() + " " + this.numberOfPlayers.get(i).getScore() + " " + this.numberOfPlayers.get(i).getColor() + "\n");
+            this.finalPanel.add(newLabel);
+        }
+        
+        this.add(finalPanel);
+        this.repaint();
+        this.validate();
+    }
 }
