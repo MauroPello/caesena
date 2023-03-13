4b6757bd1f38d10daba4993bfc3bd03871f67565
Alessandro Martini <alessandro.martini10@studio.unibo.it>
2023-03-13 14:31:27 +0100

    Update GameOverView.java

--- src/main/java/it/unibo/caesena/view/GameOverView.java
+++ src/main/java/it/unibo/caesena/view/GameOverView.java
@@ -2,32 +2,36 @@ package it.unibo.caesena.view;
 
 import java.util.List;
 import javax.swing.*;
-
 import java.awt.Component;
-
+import java.awt.Font;
 import it.unibo.caesena.model.Player;
 
 public class GameOverView extends View{
 
     private final JPanel finalPanel;
     private final List<Player> numberOfPlayers;
+    private Font mainFont;
 
     public GameOverView (GUI userInterface) {
         super(userInterface);
-        
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.mainFont = new Font("SansSerif", Font.BOLD, 20);        
         this.finalPanel = new JPanel();
         this.numberOfPlayers = userInterface.getController().getPlayers();
 
-        JLabel playersLabel = new JLabel("Players: ");
+        JLabel playersLabel = new JLabel("Game Over: ");
+        playersLabel.setFont(mainFont);
+        playersLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.finalPanel.add(playersLabel);
-
-        this.finalPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         
         for (int i = 0; i < this.numberOfPlayers.size(); i++) {
             JLabel newLabel = new JLabel(this.numberOfPlayers.get(i).toString());
+            newLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
             this.finalPanel.add(newLabel);
         }
         
+        this.finalPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         this.add(finalPanel);
         this.repaint();
         this.validate();
