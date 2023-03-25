000c17a659f851aa989974acf0a5df87378ac1bd
Davide Speziali <davide.speziali@studio.unibo.it>
2023-03-25 16:16:24 +0100

    Minor update on SectionSelectorComponentImpl class

--- src/main/java/it/unibo/caesena/view/components/SectionSelectorComponentImpl.java
+++ src/main/java/it/unibo/caesena/view/components/SectionSelectorComponentImpl.java
@@ -4,12 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.LayoutManager;
 import java.util.Optional;
-
-import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import it.unibo.caesena.model.tile.Tile;
@@ -78,16 +77,16 @@ public class SectionSelectorComponentImpl extends JPanel implements SectionSelec
         SectionButton centerButton = new SectionButton(TileSection.Center);
 
         //TODO controllare se li ho messi giusti, ho qualche dubbio
-        HorizontalSectionButtonContainer upPanel = new HorizontalSectionButtonContainer(upLeftButton, upCenterButton, upRightButton);
+        HorizontalSectionButtonContainer upPanel = new HorizontalSectionButtonContainer(upRightButton, upCenterButton, upLeftButton);
         VerticalSectionButtonContainer leftPanel = new VerticalSectionButtonContainer(leftUpButton, leftCenterButton, leftDownButton);
         VerticalSectionButtonContainer rightPanel = new VerticalSectionButtonContainer(rightUpButton, rightCenterButton, rightDownButton);
-        HorizontalSectionButtonContainer downPanel = new HorizontalSectionButtonContainer(downLeftButton, downCenterButton, downRightButton);
+        HorizontalSectionButtonContainer downPanel = new HorizontalSectionButtonContainer(downRightButton, downCenterButton, downLeftButton);
         CenterSectionButtonContainer centerPanel = new CenterSectionButtonContainer(centerButton);
 
-        this.add(upPanel, BorderLayout.SOUTH);
+        this.add(upPanel, BorderLayout.NORTH);
         this.add(leftPanel, BorderLayout.WEST);
         this.add(rightPanel, BorderLayout.EAST);
-        this.add(downPanel, BorderLayout.NORTH);
+        this.add(downPanel, BorderLayout.SOUTH);
         this.add(centerPanel, BorderLayout.CENTER);
     }
 
@@ -105,6 +104,11 @@ public class SectionSelectorComponentImpl extends JPanel implements SectionSelec
             // TODO cambiare sta roba ovviamente
             String buttonLabel = getLabelFromSection(section);
             this.setText(buttonLabel);
+
+            // this.setOpaque(false);
+            // this.setContentAreaFilled(false);
+            //this.setBorderPainted(false);
+
             this.addActionListener((e) ->
             {
                 currentSectionSelected = Optional.of(this.section);
@@ -120,21 +124,13 @@ public class SectionSelectorComponentImpl extends JPanel implements SectionSelec
     }
 
     private class VerticalSectionButtonContainer extends JPanel {
-        final Component firstComponent;
-        final Component secondComponent;
-        final Component thirdComponent;
-
         public VerticalSectionButtonContainer(Component firstComponent, Component secondComponent,
                 Component thirdComponent) {
-            this.firstComponent = firstComponent;
-            this.secondComponent = secondComponent;
-            this.thirdComponent = thirdComponent;
-
-            this.add(firstComponent);
-            this.add(Box.createVerticalStrut(10));
-            this.add(secondComponent);
-            this.add(Box.createVerticalStrut(10));
-            this.add(thirdComponent);
+            super.setOpaque(false);
+            this.setLayout(new BorderLayout());
+            this.add(new SectionButtonContainer(firstComponent), BorderLayout.SOUTH);
+            this.add(new SectionButtonContainer(secondComponent), BorderLayout.CENTER);
+            this.add(new SectionButtonContainer(thirdComponent), BorderLayout.NORTH);
         }
 
         @Override
@@ -150,41 +146,37 @@ public class SectionSelectorComponentImpl extends JPanel implements SectionSelec
     };
 
     private class HorizontalSectionButtonContainer extends JPanel {
-        final Component firstComponent;
-        final Component secondComponent;
-        final Component thirdComponent;
-
         public HorizontalSectionButtonContainer(Component firstComponent, Component secondComponent,
                 Component thirdComponent) {
-            this.firstComponent = firstComponent;
-            this.secondComponent = secondComponent;
-            this.thirdComponent = thirdComponent;
-
-            this.add(firstComponent);
-            this.add(Box.createVerticalStrut(10));
-            this.add(secondComponent);
-            this.add(Box.createVerticalStrut(10));
-            this.add(thirdComponent);
-        }
-
-        @Override
-        public void setLayout(LayoutManager mgr) {
-            mgr = new GridLayout(0,1);
-            super.setLayout(mgr);
-        }
-
-        @Override
-        public void setOpaque(boolean isOpaque) {
             super.setOpaque(false);
+            this.setLayout(new BorderLayout());
+            this.add(new SectionButtonContainer(firstComponent), BorderLayout.EAST);
+            this.add(new SectionButtonContainer(secondComponent), BorderLayout.CENTER);
+            this.add(new SectionButtonContainer(thirdComponent), BorderLayout.WEST);
         }
     };
+
+    private class SectionButtonContainer extends JPanel {
+
+        public SectionButtonContainer(Component Component) {
+            super();
+            this.setOpaque(false);
+            this.setLayout(new GridBagLayout());
+            this.add(Component);
+        }
+
+    }
 
     private class CenterSectionButtonContainer extends JPanel {
         final Component component;
 
         public CenterSectionButtonContainer(Component component) {
             this.component = component;
-            this.add(component);
+            JPanel innerPanel = new JPanel();
+            innerPanel.setLayout(new GridBagLayout());
+            innerPanel.setOpaque(false);
+            innerPanel.add(component);
+            this.add(innerPanel);
         }
 
         @Override
