25e7d3aa5f8f64d47afd9e8aaf7a467306980ca7
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-26 22:50:39 +0200

    Add confirmDialog before closing application

--- src/main/java/it/unibo/caesena/view/GUI.java
+++ src/main/java/it/unibo/caesena/view/GUI.java
@@ -6,8 +6,8 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,7 +21,7 @@ import it.unibo.caesena.utils.ImageIconUtil;
 public class GUI extends JFrame implements UserInterface {
     // TODO rimuovere
     // RAGA È SOLO PER DEBUG, SE ATTIVO UNO DISATTIVATE GLI ALTRI!!
-    private static boolean DEBUG_GAME_VIEW = true;
+    private static boolean DEBUG_GAME_VIEW = false;
     private static boolean DEBUG_GAME_OVER_VIEW = false;
     private static float MINIMUM_SIZE_RATIO = 0.35f;
     private static final String SEP = File.separator;
@@ -39,39 +39,27 @@ public class GUI extends JFrame implements UserInterface {
         this.controller = controller;
         this.players = new HashMap<>();
 
-        this.addWindowListener(new WindowListener() {
-            public void windowOpened(final WindowEvent e) {
-            }
-
-            public void windowIconified(final WindowEvent e) {
-            }
-
-            public void windowDeiconified(final WindowEvent e) {
-            }
-
-            public void windowDeactivated(final WindowEvent e) {
-            }
-
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
             public void windowClosing(final WindowEvent e) {
-                exit();
-            }
-
-            public void windowClosed(final WindowEvent e) {
-                exit();
-            }
-
-            public void windowActivated(final WindowEvent e) {
+                int result = JOptionPane.showConfirmDialog((JFrame)e.getSource(),
+                    "Are you sure you want to exit Caesena?", "Exit Caesena",
+                    JOptionPane.YES_NO_OPTION);
+ 
+                if (result == JOptionPane.YES_OPTION) {
+                    exit();
+                }
             }
         });
 
         final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setMinimumSize(new Dimension(Math.round(screenSize.width * MINIMUM_SIZE_RATIO), Math.round(screenSize.height * MINIMUM_SIZE_RATIO)));
+        this.setMinimumSize(new Dimension(Math.round(screenSize.width * MINIMUM_SIZE_RATIO), 
+            Math.round(screenSize.height * MINIMUM_SIZE_RATIO)));
         
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setLocationRelativeTo(null);
         this.setLocationByPlatform(true);
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setIconImage(ImageIconUtil.getImageFromRelativePath(ROOT + SEP + "tiles" + SEP + "TILE_BACK.png"));
         this.setVisible(true);
 
@@ -110,7 +98,8 @@ public class GUI extends JFrame implements UserInterface {
         this.gamePanel = new JPanel();
         this.gamePanel.setLayout(new OverlayLayout(this.gamePanel));
 
-        this.gamePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "togglePauseView");
+        this.gamePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+            .put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "togglePauseView");
         this.gamePanel.getActionMap().put("togglePauseView", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
