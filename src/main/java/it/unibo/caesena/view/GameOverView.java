f05374f19c042215f8b749199b28a78669c99b3c
Alessandro Martini <alessandro.martini10@studio.unibo.it>
2023-03-26 18:51:21 +0200

    Remove folder out

--- src/main/java/it/unibo/caesena/view/GameOverView.java
+++ src/main/java/it/unibo/caesena/view/GameOverView.java
@@ -8,10 +8,12 @@ import javax.swing.*;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridBagLayout;
+
+import it.unibo.caesena.controller.Controller;
 import it.unibo.caesena.model.Player;
 
 public class GameOverView extends JPanel implements View<JPanel> {
-
+    private final Controller controller;
     private final JPanel finalPanel;
     private final List<Player> players;
     private Font mainFont;
@@ -19,6 +21,7 @@ public class GameOverView extends JPanel implements View<JPanel> {
     public GameOverView (GUI userInterface) {
         super();
 
+        this.controller = userInterface.getController();
         this.setLayout(new GridBagLayout());
         this.mainFont = new Font("SansSerif", Font.BOLD, 20);        
         this.finalPanel = new JPanel();
@@ -47,8 +50,18 @@ public class GameOverView extends JPanel implements View<JPanel> {
             newLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
             this.finalPanel.add(newLabel);
         }
-        
+
+        JButton returnToStart = new JButton("Return to start Game");
+        returnToStart.addActionListener((e) -> {
+            this.controller.startGame();
+        });
+
+        JButton exitGame = new JButton("Exit Game");
+        exitGame.addActionListener(e -> {this.controller.exitGame();});
+
         this.finalPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.finalPanel.add(exitGame);
+        this.finalPanel.add(returnToStart);
         this.add(finalPanel);
         this.repaint();
         this.validate();
