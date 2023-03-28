0a0b8f7509525197877acbc55a8c3a42c6f22d2e
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-28 18:20:34 +0200

    Add javadoc for meeple package

--- src/main/java/it/unibo/caesena/model/meeple/Meeple.java
+++ src/main/java/it/unibo/caesena/model/meeple/Meeple.java
@@ -2,14 +2,34 @@ package it.unibo.caesena.model.meeple;
 
 import it.unibo.caesena.model.Player;
 
+/**
+ * An interface defining a Meeple. 
+ */
 public interface Meeple {
 
+    /**
+     * 
+     * @return The meeple strength.
+     */
     public int getStrength();
 
+    /**
+     * 
+     * @return The player that owns the meeple.
+     */
     public Player getOwner();
 
+    /**
+     * Set the meeple as placed or not.
+     * 
+     * @param placed whether the meeple is placed or not.
+     */
     public void setPlaced(final boolean placed);
 
+    /**
+     * 
+     * @return True if the meeple is placed, or else false.
+     */
     public boolean isPlaced();
 
 }
