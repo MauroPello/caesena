9c128cfefcf49391d63fcb7cec4a72a661e16e94
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-29 15:34:25 +0200

    Remove unneeded equals and hashCode

--- src/main/java/it/unibo/caesena/model/meeple/NormalMeeple.java
+++ src/main/java/it/unibo/caesena/model/meeple/NormalMeeple.java
@@ -52,38 +52,6 @@ public class NormalMeeple implements Meeple {
      * {@inheritDoc}
      */
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((owner == null) ? 0 : owner.hashCode());
-        result = prime * result + (placed ? 1 : 0);
-        return result;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-
-        final NormalMeeple other = (NormalMeeple) obj;
-        return owner.equals(other.getOwner())
-                && placed == other.isPlaced();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
     public String toString() {
         return new StringUtil.ToStringBuilder().addFromObjectGetters(this).build();
     }
