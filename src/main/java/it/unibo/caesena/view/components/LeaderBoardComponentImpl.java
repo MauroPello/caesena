6b0bbff87d7ebfcf9487b80a532f2445d02b53f6
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-03-30 15:33:16 +0200

    Fix leaderboard showing wrong colors and not updating correctly

--- src/main/java/it/unibo/caesena/view/components/LeaderBoardComponentImpl.java
+++ src/main/java/it/unibo/caesena/view/components/LeaderBoardComponentImpl.java
@@ -3,7 +3,6 @@ package it.unibo.caesena.view.components;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BoxLayout;
@@ -18,9 +17,9 @@ public class LeaderBoardComponentImpl extends JPanel implements LeaderBoardCompo
 
     private static final int WIDTH = 10;
     private static final int HEIGHT = 10;
-    List<JLabel> labelList = new ArrayList<>();
-    List<Player> playerList = new ArrayList<>();
+    
     private final GUI userInterface;
+    private final JPanel playersPanel;
 
     public LeaderBoardComponentImpl(final GUI userInterface) {
         super();
@@ -28,7 +27,6 @@ public class LeaderBoardComponentImpl extends JPanel implements LeaderBoardCompo
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.userInterface = userInterface;
-        this.playerList = this.userInterface.getController().getPlayers();
 
         final JLabel titleLabel = new JLabel("Leaderboard:");
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -39,32 +37,35 @@ public class LeaderBoardComponentImpl extends JPanel implements LeaderBoardCompo
         separator.setForeground(Color.black);
         this.add(separator);
 
-        for (final var player : playerList) {
-            final var playerPanel = new JPanel();
+        this.playersPanel = new JPanel();
+        this.playersPanel.setLayout(new BoxLayout(playersPanel, BoxLayout.Y_AXIS));
+        this.add(playersPanel);
 
-            final var playerColorPanel = new PlayerImageImpl(WIDTH, HEIGHT);
-            playerColorPanel.setColor(userInterface.getPlayerColor(player));
-
-            final JLabel playerLabel = new JLabel();
-            playerLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
-            playerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-            labelList.add(playerLabel);
-
-            playerPanel.add(playerColorPanel);
-            playerPanel.add(playerLabel);
-            this.add(playerPanel);
-        }
         updateLeaderBoard();
         this.setVisible(true);
     }
 
     @Override
     public void updateLeaderBoard() {
-        this.playerList = new ArrayList<>(this.userInterface.getController().getPlayers());
-        playerList.sort((x, y) -> Integer.compare(y.getScore(), x.getScore()));
+        playersPanel.removeAll();
+        playersPanel.revalidate();
+        playersPanel.repaint();
 
-        for (int i = 0; i < playerList.size(); i++) {
-            labelList.get(i).setText(playerList.get(i).getName() + " " + playerList.get(i).getScore());
+        final List<Player> players = userInterface.getController().getPlayers().stream()
+            .sorted((p1, p2) -> Integer.compare(p1.getScore(), p2.getScore())).toList();
+        for (final var player : players) {
+            final var playerPanel = new JPanel();
+
+            final var playerColorPanel = new PlayerImageImpl(WIDTH, HEIGHT);
+            playerColorPanel.setColor(userInterface.getPlayerColor(player));
+
+            final JLabel playerLabel = new JLabel(player.getName() + " " + player.getScore());
+            playerLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+            playerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            
+            playerPanel.add(playerColorPanel);
+            playerPanel.add(playerLabel);
+            playersPanel.add(playerPanel);
         }
     }
 
