fca40f2edc4b58467fffa0c45bf0cc2406135f42
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-04-01 00:31:23 +0200

    Improve PauseView button arrangement and display

--- src/main/java/it/unibo/caesena/view/PauseView.java
+++ src/main/java/it/unibo/caesena/view/PauseView.java
@@ -4,27 +4,29 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PauseView extends JPanel implements View<JPanel> {
-
+    private static final float MODAL_RESIZE_RATIO = 0.5f;
     private static final Color BACKGROUND_COLOR = new Color(255, 255, 255, 80);
-    private final GUI userInterface;
     private static final int DEFAULT_SIZE = 20;
+    private static final int PADDING_SIZE = 10;
+    private final GUI userInterface;
 
     public PauseView(final GUI userInterface) {
         super();
         this.userInterface = userInterface;
 
-        this.setLayout(new GridBagLayout());
         this.setBackground(BACKGROUND_COLOR);
-
         final Font mainFont = new Font(Font.SANS_SERIF, Font.BOLD, DEFAULT_SIZE);
 
         final JPanel mainPanel = new JPanel();
@@ -32,35 +34,49 @@ public class PauseView extends JPanel implements View<JPanel> {
 
         final JLabel titleLbl = new JLabel(LocaleHelper.getViewTitle("PauseView", false));
         titleLbl.setFont(mainFont);
+        titleLbl.setBorder(BorderFactory.createEmptyBorder(PADDING_SIZE, 0, PADDING_SIZE, 0));
         titleLbl.setAlignmentX(Component.CENTER_ALIGNMENT);
-        mainPanel.add(titleLbl);
+
+        final JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.insets = new Insets(PADDING_SIZE, 0, PADDING_SIZE, 0);
 
         final JButton resumeGameButton = new JButton(LocaleHelper.getResumeGameText());
         resumeGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         resumeGameButton.addActionListener((e) -> {
             userInterface.togglePauseView();
         });
-        mainPanel.add(resumeGameButton);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        buttonsPanel.add(resumeGameButton, gridBagConstraints);
 
         final JButton backToStartMenuButton = new JButton(LocaleHelper.getBackToStartMenuText());
         backToStartMenuButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         backToStartMenuButton.addActionListener((e) -> {
             userInterface.showBackToStartViewDialog();
         });
-        mainPanel.add(backToStartMenuButton);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        buttonsPanel.add(backToStartMenuButton, gridBagConstraints);
 
         final JButton exitButton = new JButton(LocaleHelper.getExitApplicationText());
         exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         exitButton.addActionListener((e) -> {
             userInterface.showExitDialog();
         });
-        mainPanel.add(exitButton);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        buttonsPanel.add(exitButton, gridBagConstraints);
 
         final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        mainPanel.setPreferredSize(new Dimension((int) Math.round(screenSize.getWidth() * GUI.MODAL_PREFERRED_RATIO / 2), (int) Math.round(screenSize.getHeight() * GUI.MODAL_PREFERRED_RATIO / 2)));
-        mainPanel.setMinimumSize(new Dimension((int) Math.round(screenSize.getWidth() * GUI.MODAL_MINIMUM_RATIO / 2), (int) Math.round(screenSize.getHeight() * GUI.MODAL_MINIMUM_RATIO / 2)));
-        mainPanel.setMaximumSize(new Dimension((int) Math.round(screenSize.getWidth() * GUI.MODAL_MAXIMUM_RATIO / 2), (int) Math.round(screenSize.getHeight() * GUI.MODAL_MAXIMUM_RATIO / 2)));
+        mainPanel.setPreferredSize(new Dimension((int) Math.round(screenSize.getWidth() * GUI.MODAL_PREFERRED_RATIO * MODAL_RESIZE_RATIO), (int) Math.round(screenSize.getHeight() * GUI.MODAL_PREFERRED_RATIO * MODAL_RESIZE_RATIO)));
+        mainPanel.setMinimumSize(new Dimension((int) Math.round(screenSize.getWidth() * GUI.MODAL_MINIMUM_RATIO * MODAL_RESIZE_RATIO), (int) Math.round(screenSize.getHeight() * GUI.MODAL_MINIMUM_RATIO * MODAL_RESIZE_RATIO)));
+        mainPanel.setMaximumSize(new Dimension((int) Math.round(screenSize.getWidth() * GUI.MODAL_MAXIMUM_RATIO * MODAL_RESIZE_RATIO), (int) Math.round(screenSize.getHeight() * GUI.MODAL_MAXIMUM_RATIO * MODAL_RESIZE_RATIO)));
 
+        mainPanel.add(titleLbl);
+        mainPanel.add(buttonsPanel);
+        this.setLayout(new GridBagLayout());
         this.add(mainPanel);
     }
 
