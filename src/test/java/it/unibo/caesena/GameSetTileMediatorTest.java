3ad1d015eb2cf57b3ea72db98ef44224f38a7704
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-04-05 15:43:26 +0200

    Create GameSetTileMediatorTest and move into it testRotation from TileTest

---
+++ src/test/java/it/unibo/caesena/GameSetTileMediatorTest.java
@@ -0,0 +1,75 @@
+package it.unibo.caesena;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.caesena.model.GameSetTileMediator;
+import it.unibo.caesena.model.GameSetTileMediatorImpl;
+import it.unibo.caesena.model.gameset.GameSet;
+import it.unibo.caesena.model.gameset.GameSetFactoryImpl;
+import it.unibo.caesena.model.gameset.GameSetType;
+import it.unibo.caesena.model.tile.MutableTile;
+import it.unibo.caesena.model.tile.TileBuilder;
+import it.unibo.caesena.model.tile.TileFactoryWithBuilder;
+import it.unibo.caesena.model.tile.TileSection;
+import it.unibo.caesena.model.tile.TileType;
+import it.unibo.caesena.utils.Pair;
+
+public class GameSetTileMediatorTest {
+
+    private static Pair<MutableTile,Map<GameSet,Set<TileSection>>> tileMap;
+    private static GameSetTileMediator mediator;
+
+    @BeforeAll
+    static void init() {
+        mediator = new GameSetTileMediatorImpl(new GameSetFactoryImpl());
+        tileMap = new TileFactoryWithBuilder().createCityEdge();
+        tileMap.getY().forEach((k,v) -> mediator.addSections(k, tileMap.getX(), v));
+    }
+
+    @Test
+    void testGetGameSetInSection() {
+        assertEquals(GameSetType.FIELD, mediator.getGameSetInSection(tileMap.getX(), TileSection.CENTER).getType());
+    }
+
+    @Test
+    void testGetGameSetsInTile() {
+        assertEquals(tileMap.getY().keySet(), mediator.getGameSetsInTile(tileMap.getX()));
+    }
+
+    @Test
+    void testGetAllGameSets() {
+        assertEquals(tileMap.getY().keySet(), mediator.getAllGameSets());
+    }
+
+    @Test
+    void testGetTilesFromGameSet() {
+        for (final var gameSet : tileMap.getY().keySet()) {
+            assertTrue(mediator.getTilesFromGameSet(gameSet).keySet().contains(tileMap.getX()));
+        }
+    }
+
+    @Test
+    void testRotateTileClockwise() {
+        mediator.rotateTileClockwise(tileMap.getX());
+        assertEquals(1, tileMap.getX().getRotationCount());
+        final Pair<MutableTile, Map<GameSet, Set<TileSection>>> tile2 = new TileBuilder(TileType.CITY_EDGE)
+                .city(Set.of(TileSection.RIGHT_UP, TileSection.RIGHT_CENTER, TileSection.RIGHT_DOWN,
+                        TileSection.DOWN_LEFT, TileSection.DOWN_CENTER, TileSection.DOWN_RIGHT))
+                .field(Set.of(TileSection.UP_RIGHT, TileSection.UP_CENTER, TileSection.UP_LEFT,
+                        TileSection.LEFT_UP, TileSection.LEFT_CENTER, TileSection.LEFT_DOWN, TileSection.CENTER))
+                .build();
+
+        for (final var entry : tile2.getY().entrySet()) {
+            for (final var section : entry.getValue()) {
+                assertEquals(entry.getKey().getType(), mediator.getGameSetInSection(tileMap.getX(), section).getType());
+            }
+        }
+    }
+}
--- src/test/java/it/unibo/caesena/TileTest.java
+++ src/test/java/it/unibo/caesena/TileTest.java
@@ -4,62 +4,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Map;
-import java.util.Set;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.caesena.model.GameSetTileMediator;
 import it.unibo.caesena.model.GameSetTileMediatorImpl;
-import it.unibo.caesena.model.gameset.GameSet;
 import it.unibo.caesena.model.gameset.GameSetFactoryImpl;
 import it.unibo.caesena.model.tile.MutableTile;
-import it.unibo.caesena.model.tile.TileBuilder;
 import it.unibo.caesena.model.tile.TileFactoryWithBuilder;
 import it.unibo.caesena.model.tile.TileSection;
-import it.unibo.caesena.model.tile.TileType;
 import it.unibo.caesena.utils.Pair;
 
 final class TileTest {
 
     private static MutableTile tile;
-    private static GameSetTileMediator mediator;
-    private static Pair<Integer, Integer> position;
 
     @BeforeAll
     static void init() {
-        mediator = new GameSetTileMediatorImpl(new GameSetFactoryImpl());
+        final GameSetTileMediator mediator = new GameSetTileMediatorImpl(new GameSetFactoryImpl());
         final var tileMap = new TileFactoryWithBuilder().createCityEdge();
         tile = tileMap.getX();
         tileMap.getY().forEach((k,v) -> mediator.addSections(k, tile, v));
-        position = new Pair<>(1, 1);
     }
 
     @Test
     void testGetters() {
+        final Pair<Integer, Integer> position = new Pair<>(1, 1);
         assertFalse(tile.isPlaced());
         tile.setPosition(position);
         assertTrue(tile.isPlaced());
         assertEquals(position, tile.getPosition().get());
-    }
-
-    @Test
-    void testRotation() {
-        mediator.rotateTileClockwise(tile);
-        assertEquals(tile.getRotationCount() , 1);
-        final Pair<MutableTile, Map<GameSet, Set<TileSection>>> tile2 = new TileBuilder(TileType.CITY_EDGE)
-                .city(Set.of(TileSection.RIGHT_UP, TileSection.RIGHT_CENTER, TileSection.RIGHT_DOWN,
-                        TileSection.DOWN_LEFT, TileSection.DOWN_CENTER, TileSection.DOWN_RIGHT))
-                .field(Set.of(TileSection.UP_RIGHT, TileSection.UP_CENTER, TileSection.UP_LEFT,
-                        TileSection.LEFT_UP, TileSection.LEFT_CENTER, TileSection.LEFT_DOWN, TileSection.CENTER))
-                .build();
-
-        for (final var entry : tile2.getY().entrySet()) {
-            for (final var section : entry.getValue()) {
-                assertEquals(entry.getKey().getType(), mediator.getGameSetInSection(tile, section).getType());
-            }
-        }
     }
 
     @Test
