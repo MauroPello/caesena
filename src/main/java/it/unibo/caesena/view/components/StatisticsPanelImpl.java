aacd12878b8bf082efdd47939a38b7c7a1fd7ad9
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-05-14 12:28:06 +0200

    Add scrollable panes

--- src/main/java/it/unibo/caesena/view/components/StatisticsPanelImpl.java
+++ src/main/java/it/unibo/caesena/view/components/StatisticsPanelImpl.java
@@ -1,18 +1,29 @@
 package it.unibo.caesena.view.components;
 
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import it.unibo.caesena.view.UserInterface;
 
 public class StatisticsPanelImpl implements StatisticsPanel<JPanel> {
 
     private final JPanel mainPanel;
+    private final JPanel contentPanel;
 
     public StatisticsPanelImpl(final UserInterface userInterface) {
         this.mainPanel = new JPanel();
         this.mainPanel.setOpaque(false);
 
-        // code
+        this.contentPanel = new JPanel();
+        this.contentPanel.setLayout(new BoxLayout(this.contentPanel, BoxLayout.Y_AXIS));
+        this.contentPanel.setOpaque(false);
+        final JScrollPane contentScrollPane = new JScrollPane(contentPanel);
+        contentScrollPane.setOpaque(false);
+        contentScrollPane.getViewport().setOpaque(false);
+        contentScrollPane.setAutoscrolls(true);
+        contentScrollPane.setBorder(null);
+        this.mainPanel.add(contentScrollPane);
 
         this.mainPanel.setVisible(false);
     }
