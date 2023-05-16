f0dd0d616e13cd0836e2611821e0c86c55611e07
Davide Speziali <davide.speziali@studio.unibo.it>
2023-05-16 16:23:07 +0200

    Fix region

--- src/main/java/it/unibo/caesena/model/server/Region.java
+++ src/main/java/it/unibo/caesena/model/server/Region.java
@@ -3,13 +3,7 @@ package it.unibo.caesena.model.server;
 import java.util.List;
 
 public class Region {
-    private final int regionID;
-    private final Continent continent;
-    private final List<Region> regions;
-
-    public Region(int regionID, Continent continent, List<Region> regions) {
-        this.regionID = regionID;
-        this.continent = continent;
-        this.regions = regions;
-    }
+    private int regionID;
+    private Continent continent;
+    private List<Server> Servers;
 }
