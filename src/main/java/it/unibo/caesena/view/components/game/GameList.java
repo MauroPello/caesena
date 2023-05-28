05a9dad4005080f10f7a7666ab7b0d7945edd19d
Davide Speziali <davide.speziali@studio.unibo.it>
2023-05-28 17:56:26 +0200

    finish view for game selection

---
+++ src/main/java/it/unibo/caesena/view/components/game/GameList.java
@@ -0,0 +1,91 @@
+package it.unibo.caesena.view.components.game;
+
+import it.unibo.caesena.model.Game;
+import it.unibo.caesena.model.player.Player;
+import it.unibo.caesena.model.player.PlayerInGameImpl;
+import it.unibo.caesena.model.server.Server;
+import it.unibo.caesena.view.BasicComponent;
+import it.unibo.caesena.view.UserInterface;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.*;
+
+public class GameList implements BasicComponent<JPanel> {
+
+    private final Player player;
+    private final UserInterface ui;
+    private final JPanel mainPanel;
+    private final Map<JButton, Game> gameButtonMap;
+
+    public GameList(Player player, UserInterface ui) {
+        this.player = player;
+        this.ui = ui;
+        this.gameButtonMap = new HashMap<>();
+        this.mainPanel = new JPanel();
+        this.mainPanel.setLayout(new GridLayout(0, 5));
+
+        var games = this.ui.getController().getOpenGames(this.player);
+
+        String[] header = { "Game ID", "Players", "Remaining Tiles", "Server", "" };
+
+        this.mainPanel.add(new JLabel(header[0]));
+        this.mainPanel.add(new JLabel(header[1]));
+        this.mainPanel.add(new JLabel(header[2]));
+        this.mainPanel.add(new JLabel(header[3]));
+        this.mainPanel.add(new JLabel(header[4]));
+
+        for (Game game : games) {
+
+            JLabel playersLabel = new JLabel();
+            List<PlayerInGameImpl> playersInGame = game.getPlayersInGame();
+            String playersLabelText = playersInGame.stream()
+                    .map(p -> p.getPlayer().getName())
+                    .collect(Collectors.joining(", "));
+            playersLabel.setText(playersLabelText);
+            JLabel serverLabel = new JLabel();
+            Server server = game.getServer();
+            serverLabel.setText(server.toString());
+            JLabel gameIDLabel = new JLabel(String.valueOf(game.getGameID()));
+            JLabel remainingTilesLabel = new JLabel("da fare ancora");
+            JButton joinButton = new JButton("join");
+            joinButton.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    JButton selectedButton = (JButton) arg0.getSource();
+                    ui.getController().joinGame(gameButtonMap.get(selectedButton).getGameID());
+                }
+
+            });
+            gameButtonMap.put(joinButton, game);
+
+            this.mainPanel.add(gameIDLabel);
+            this.mainPanel.add(playersLabel);
+            this.mainPanel.add(remainingTilesLabel);
+            this.mainPanel.add(serverLabel);
+            this.mainPanel.add(joinButton);
+        }
+    }
+
+    @Override
+    public boolean isVisible() {
+        return true;
+    }
+
+    @Override
+    public void setVisible(boolean visible) {
+
+    }
+
+    @Override
+    public JPanel getComponent() {
+        return this.mainPanel;
+    }
+
+}
--- src/main/java/it/unibo/caesena/view/components/game/GameListPanelImpl.java
+++ src/main/java/it/unibo/caesena/view/components/game/GameListPanelImpl.java
@@ -3,18 +3,10 @@ package it.unibo.caesena.view.components.game;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
-import it.unibo.caesena.model.Game;
 import it.unibo.caesena.model.player.Player;
-import it.unibo.caesena.model.player.PlayerInGameImpl;
-import it.unibo.caesena.model.server.Server;
 import it.unibo.caesena.view.GUI;
 import it.unibo.caesena.view.UserInterface;
 
@@ -22,31 +14,30 @@ public class GameListPanelImpl implements GameListPanel<JPanel> {
 
     private final JPanel mainPanel;
     private final JPanel contentPanel;
+    private final JPanel innerContentPanel;
     private final UserInterface ui;
-    private final DefaultTableModel gameList;
     private Player selectedPlayer;
 
     public GameListPanelImpl(final UserInterface userInterface) {
+        this.ui = userInterface;
         this.mainPanel = new JPanel();
         this.mainPanel.setOpaque(false);
-        this.ui = userInterface;
+        this.innerContentPanel = new JPanel();
+        this.innerContentPanel.setOpaque(false);
 
         this.contentPanel = new JPanel();
-        this.contentPanel.setLayout(new BoxLayout(this.contentPanel, BoxLayout.X_AXIS));
+        this.contentPanel.setLayout(new BoxLayout(this.contentPanel, BoxLayout.Y_AXIS));
         this.contentPanel.setOpaque(false);
 
         final JComboBox<String> playerChooser = new JComboBox<>();
         final List<Player> players = userInterface.getController().getAllPlayers();
-        if(!players.isEmpty()) {
+        if (!players.isEmpty()) {
             players.forEach(s -> playerChooser.addItem(s.getName()));
             playerChooser.setSelectedIndex(0);
             playerChooser.setFont(GUI.MEDIUM_BOLD_FONT);
-            mainPanel.add(playerChooser);
+            this.contentPanel.add(playerChooser);
         }
-        Object[] header = { "Game ID", "Players", "Remaining Tiles", "Server" };
-        gameList = new DefaultTableModel(header, 0);
-        JTable gameListTable = new JTable(gameList);
-        this.contentPanel.add(gameListTable);
+
         playerChooser.addItemListener(new ItemListener() {
 
             @Override
@@ -56,6 +47,7 @@ public class GameListPanelImpl implements GameListPanel<JPanel> {
             }
 
         });
+        this.contentPanel.add(this.innerContentPanel);
         this.mainPanel.add(contentPanel);
         this.mainPanel.setVisible(false);
     }
@@ -82,31 +74,32 @@ public class GameListPanelImpl implements GameListPanel<JPanel> {
     @Override
     public void update() {
         if (selectedPlayer != null) {
-            var games = this.ui.getController().getOpenGames(selectedPlayer);
+            // var games = this.ui.getController().getOpenGames(selectedPlayer);
 
-            for (int i = 0; i < gameList.getRowCount(); i++) {
-                gameList.removeRow(i);
-            }
+            // for (int i = 0; i < gameList.getRowCount(); i++) {
+            // gameList.removeRow(i);
+            // }
 
-            for (Game game : games) {
-                JPanel gameInfo = new JPanel();
-                JLabel playersLabel = new JLabel();
-                List<PlayerInGameImpl> playersInGame = game.getPlayersInGame();
-                String playersLabelText = playersInGame.stream()
-                        .map(p -> p.getPlayer().getName())
-                        .collect(Collectors.joining(", "));
-                playersLabel.setText(playersLabelText);
-                JLabel serverLabel = new JLabel();
-                Server server = game.getServer();
-                serverLabel.setText(server.toString());
+            // for (Game game : games) {
+            // JPanel gameInfo = new JPanel();
+            // JLabel playersLabel = new JLabel();
+            // List<PlayerInGameImpl> playersInGame = game.getPlayersInGame();
+            // String playersLabelText = playersInGame.stream()
+            // .map(p -> p.getPlayer().getName())
+            // .collect(Collectors.joining(", "));
+            // playersLabel.setText(playersLabelText);
+            // JLabel serverLabel = new JLabel();
+            // Server server = game.getServer();
+            // serverLabel.setText(server.toString());
 
-                gameInfo.add(playersLabel);
-                gameInfo.add(serverLabel);
-                Object[] data = { String.valueOf(game.getGameID()), playersLabelText, "Non l'ho fatto ancora",
-                        server.toString() };
-                gameList.addRow(data);
-            }
-
+            // gameInfo.add(playersLabel);
+            // gameInfo.add(serverLabel);
+            // Object[] data = { String.valueOf(game.getGameID()), playersLabelText, "Non
+            // l'ho fatto ancora",
+            // server.toString() };
+            // gameList.addRow(data);
+            this.innerContentPanel.removeAll();
+            this.innerContentPanel.add((new GameList(selectedPlayer, ui)).getComponent());
             this.mainPanel.repaint();
             this.mainPanel.revalidate();
         }
